791ced377e812dbd8f5631457a016b3b502b197d
MaxOhn <ohn.m@hotmail.de>
2023-01-03 20:04:35 +0100

    improved path handling

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,23 @@
 #![allow(dead_code)]
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::fs::File;
-use std::io::{BufReader};
-use std::path::Path;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Configuration {
-    pub browser_path: String,
-    pub custom_osu_path: Option<String>,
+    #[serde(deserialize_with = "deserialize_path")]
+    pub browser_path: Option<PathBuf>,
+    #[serde(deserialize_with = "deserialize_path")]
+    pub custom_osu_path: Option<PathBuf>,
 }
 
 impl Configuration {
     pub fn empty() -> Self {
         Self {
-            browser_path: "auto".into(),
-            custom_osu_path: Some("".into()),
+            browser_path: None,
+            custom_osu_path: None,
         }
     }
 
@@ -30,5 +32,15 @@ impl Configuration {
         std::fs::write(path, serde_json::to_string(&Configuration::empty())?)?;
 
         Ok(())
+    }
+}
+
+fn deserialize_path<'de, D: Deserializer<'de>>(d: D) -> Result<Option<PathBuf>, D::Error> {
+    let path = <Option<String> as Deserialize>::deserialize(d)?;
+
+    if let Some("auto" | "") = path.as_deref() {
+        Ok(None)
+    } else {
+        Ok(path.map(PathBuf::from))
     }
 }
